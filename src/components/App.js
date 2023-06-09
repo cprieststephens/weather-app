b56896b77b9ed36f80d6317f72604382c1ac5464
Camilla Prist-Stephens <cprieststephens@googlemail.com>
2023-06-09 14:42:54 +0100

    Add Forecast Summaries component and props

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import "../styles/App.css";
 import LocationDetails from "./LocationDetails";
+import ForecastSummaries from "./ForecastSummaries";
 
-function App({ location }) {
+function App({ forecasts, location }) {
   const { city, country } = location;
-  return <LocationDetails city={city} country={country} />;
+  return (
+    <div>
+      <LocationDetails city={city} country={country} />
+      <ForecastSummaries forecasts={forecasts} />
+    </div>
+  );
 }
 
 export default App;
