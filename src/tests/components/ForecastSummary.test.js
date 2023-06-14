588fca4e187455259f796c050cb24d0f95fc88d1
Camilla Prist-Stephens <cprieststephens@googlemail.com>
2023-06-14 06:55:17 +0100

    Updated snapshots and fixed test for Forecast Summary component

--- src/tests/components/ForecastSummary.test.js
+++ src/tests/components/ForecastSummary.test.js
@@ -10,6 +10,7 @@ describe("ForecastSummary", () => {
       min: 12,
       max: 22,
     },
+    onSelect: () => {},
   };
 
   it("renders correctly", () => {
@@ -19,6 +20,7 @@ describe("ForecastSummary", () => {
         description={validProps.description}
         icon={validProps.icon}
         temperature={validProps.temperature}
+        onSelect={validProps.onSelect}
       />
     );
 
