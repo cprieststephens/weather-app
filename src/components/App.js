67469eb5c19260520713d8c182910cbaef38449d
Camilla Prist-Stephens <cprieststephens@googlemail.com>
2023-06-17 07:37:10 +0100

    Add Search Form component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 
 import LocationDetails from "./LocationDetails";
+import SearchForm from "./SearchForm";
 import ForecastSummaries from "./ForecastSummaries";
 import ForecastDetails from "./ForecastDetails";
 import getForecast from "../requests/getForecast";
@@ -11,24 +12,34 @@ function App() {
   const [forecasts, setForecasts] = useState([]);
   const [location, setLocation] = useState({ city: "", country: "" });
   const [selectedDate, setSelectedDate] = useState(0);
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
     getForecast(setSelectedDate, setForecasts, setLocation);
   }, []);
 
-  const selectedForecast = forecasts.find(
-    (forecast) => forecast.date === selectedDate
-  );
-
   const handleForecastSelect = (date) => {
     setSelectedDate(date);
   };
+
+  const handleCitySearch = () => {
+    getForecast(setSelectedDate, setForecasts, setLocation);
+  };
+
+  const selectedForecast = forecasts.find(
+    (forecast) => forecast.date === selectedDate
+  );
 
   const { city, country } = location;
 
   return (
     <div className="weather-app">
       <LocationDetails city={city} country={country} />
+      <SearchForm
+        searchText={searchText}
+        setSearchText={setSearchText}
+        onSubmit={handleCitySearch}
+      />
       <ForecastSummaries
         forecasts={forecasts}
         onForecastSelect={handleForecastSelect}
---
+++ src/components/SearchForm.js
@@ -0,0 +1,13 @@
+function SearchForm({ searchText, setSearchText, onSubmit }) {
+  const handleInputChange = (event) => setSearchText(event.target.value);
+  return (
+    <div className="search-form">
+      <input type="text" onChange={handleInputChange} value={searchText} />
+      <button type="submit" onClick={onSubmit}>
+        Search
+      </button>
+    </div>
+  );
+}
+
+export default SearchForm;
