f1a191992ce01dc217eb4d46c9b70554e0529793
Camilla Prist-Stephens <cprieststephens@googlemail.com>
2023-08-09 07:35:48 +0100

    Update CSS styling

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -4,7 +4,7 @@ function SearchForm({ searchText, setSearchText, onSubmit }) {
   const handleInputChange = (event) => setSearchText(event.target.value);
   return (
     <div className="search-form">
-      <input type="text" onChange={handleInputChange} value={searchText} />
+      <input type="search" onChange={handleInputChange} value={searchText} />
       <button type="submit" onClick={onSubmit}>
         Search
       </button>
