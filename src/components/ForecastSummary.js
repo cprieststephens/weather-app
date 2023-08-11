e3bafc0ae799d34e5a106b6d0f05df7e822ee2ec
Camilla Prist-Stephens <cprieststephens@googlemail.com>
2023-08-11 12:06:44 +0100

    Refactor ForecastSummary component

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -5,28 +5,15 @@ function ForecastSummary({ date, description, icon, onSelect, temperature }) {
   const formattedDate = new Date(date).toDateString();
   const weatherCode = `${icon.slice(0, 1)}00`;
 
-  if (icon > 800) {
-    return (
-      <div className="forecast-summary" data-testid="forecast-summary">
-        <div className="forecast-summary__date">{formattedDate}</div>
-        <div className="forecast-summary__icon" data-testid="forecast-icon">
-          <img src={iconData[802]} alt="icon" />
-        </div>
-        <div className="forecast-summary__temperature">
-          {temperature.max} &deg;C
-        </div>
-        <div className="forecast-summary__description">{description}</div>
-        <button type="button" onClick={() => onSelect(date)}>
-          More details:
-        </button>
-      </div>
-    );
-  }
   return (
     <div className="forecast-summary" data-testid="forecast-summary">
       <div className="forecast-summary__date">{formattedDate}</div>
       <div className="forecast-summary__icon" data-testid="forecast-icon">
-        <img src={iconData[weatherCode]} alt="icon" />
+        {icon > 800 ? (
+          <img src={iconData[802]} alt={description} />
+        ) : (
+          <img src={iconData[weatherCode]} alt={description} />
+        )}
       </div>
       <div className="forecast-summary__temperature">
         {temperature.max} &deg;C
@@ -38,4 +25,5 @@ function ForecastSummary({ date, description, icon, onSelect, temperature }) {
     </div>
   );
 }
+
 export default ForecastSummary;
